174d5bc8c54c24d233eb51cf601d3ddc7e7c16ee
Dmitry Lyapin <lyapindm@yandex.ru>
2021-03-05 14:27:19 +0300

    Валидация форм v.1

--- static/src/template-parts/form-sign-up.tmpl.ts
+++ static/src/template-parts/form-sign-up.tmpl.ts
@@ -6,27 +6,27 @@ const formSignUp:Template = (() => {
         <h1 class="form-window__title">Регистрация</h1>\
         <form class="form form__signup" name="signup">\
           <div class="form__wrap">\
-            <label class="form__label" for="email-signup">Почта</label>\
+            <label class="form__label form__label_hidden" for="email-signup">Почта</label>\
             <input class="input" name="email" id="email-signup" type="email" placeholder="Почта" pattern="<%=email%>">\
-            <span class="form__error form__error_email">Неверная почта</span>\
-            <label class="form__label" for="login-signup">Логин</label>\
+            <span class="form__error form__error_email"></span>\
+            <label class="form__label form__label_hidden" for="login-signup">Логин</label>\
             <input class="input" name="login" id="login-signup" type="text" placeholder="Логин" required minlength="2" maxlength="30">\
-            <span class="form__error form__error_login">Неверный логин</span>\
-            <label class="form__label" for="first_name-signup">Имя</label>\
+            <span class="form__error form__error_login"></span>\
+            <label class="form__label form__label_hidden" for="first_name-signup">Имя</label>\
             <input class="input" name="first_name" id="first_name-signup" type="text" placeholder="Имя" required minlength="2" maxlength="30">\
-            <span class="form__error form__error_first_name">Неверное имя</span>\
-            <label class="form__label" for="second_name-signup">Фамилия</label>\
+            <span class="form__error form__error_first_name"></span>\
+            <label class="form__label form__label_hidden" for="second_name-signup">Фамилия</label>\
             <input class="input" name="second_name" id="second_name-signup" type="text" placeholder="Фамилия" required minlength="2" maxlength="30">\
-            <span class="form__error form__error_second_name">Неверное фамилие</span>\
-            <label class="form__label" for="phone-signup">Телефон</label>\
+            <span class="form__error form__error_second_name"></span>\
+            <label class="form__label form__label_hidden" for="phone-signup">Телефон</label>\
             <input class="input" name="phone" id="phone-signup" type="tel" placeholder="Телефон" pattern="<%=phone%>">\
-            <span class="form__error form__error_phone">Неверный телефон</span>\
-            <label class="form__label" for="password-signup">Пароль</label>\
+            <span class="form__error form__error_phone"></span>\
+            <label class="form__label form__label_hidden" for="password-signup">Пароль</label>\
             <input class="input" name="password" id="password-signup" type="password" placeholder="Пароль" required  minlength="6">\
-            <span class="form__error form__error_password">Неверный пароль</span>\
-            <label class="form__label" for="second-password-signup">Пароль</label>\
+            <span class="form__error form__error_password"></span>\
+            <label class="form__label form__label_hidden" for="second-password-signup">Пароль</label>\
             <input class="input" name="password" id="second-password-signup" type="password" placeholder="Пароль" required  minlength="6">\
-            <span class="form__error form__error_password">Неверный пароль</span>\
+            <span class="form__error form__error_password"></span>\
           </div>\
           <div class="form__wrap">\
             <button class="button">Зарегистрироваться</button>\
---
+++ static/src/types/InputErrors.ts
@@ -0,0 +1,8 @@
+interface InputErrors {
+ ERROR_TEXT: string;
+ ERROR_EMAIL: string;
+ ERROR_PASSWORD: string;
+ ERROR_PASSWORDS: string;
+ ERROR_TEL: string;
+ ERROR_REQUIRED_FIELD: string;
+}
